3ebdaceed53e44b1f3a00856cd9b5811f8a18673
Abu-Sufian Vagabov <48068274+Abu-SufianV@users.noreply.github.com>
2019-03-06 21:37:34 +0300

    Update Equation.py

--- HW 28.02/Equation.py
+++ HW 28.02/Equation.py
@@ -1,1 +1,20 @@
+#Ax^2+Bx+C=0
 
+import math
+
+A=int(input("Введите переменную А: "))
+B=int(input("Введите переменную B: "))
+C=int(input("Введите переменную C: "))
+
+print ("\n\nA =", A, "\nB =", B,"\nС =" , C)
+
+D = B*B - 4*A*C
+if D>0:
+    x1=(-B+math.sqrt(D))/(2*A)
+    x2=(-B-math.sqrt(D))/(2*A)
+    print("D =", D, "\nПевый корень:", x1, "\nВторой корень: ", x2)
+elif D==0:
+    x=-B/(2*A)
+    print("D =", D, "\nКорень:", x)
+else:
+    print("Корней нет")

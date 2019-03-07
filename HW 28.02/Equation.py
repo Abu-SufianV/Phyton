ed7ad5154176cda8b23c4a65f19ae5e86ae729ae
Abu-Sufian Vagabov <48068274+Abu-SufianV@users.noreply.github.com>
2019-03-08 00:08:57 +0300

    Update Equation.py
    Вроде должно быть в порядке

--- HW 28.02/Equation.py
+++ HW 28.02/Equation.py
@@ -9,7 +9,10 @@ C=int(input("Введите переменную C: "))
 print ("\n\nA =", A, "\nB =", B,"\nС =" , C)
 
 D = B*B - 4*A*C
-if D>0:
+if A==0:
+    x=-C/B
+    print("Корень уравнения: ", x)
+elif D>0:
     x1=(-B+math.sqrt(D))/(2*A)
     x2=(-B-math.sqrt(D))/(2*A)
     print("D =", D, "\nПевый корень:", x1, "\nВторой корень: ", x2)

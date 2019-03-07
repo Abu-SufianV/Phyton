b2c85b8f8df2bd6fe58ec05edcafc8f955409bb9
Abu-Sufian Vagabov <48068274+Abu-SufianV@users.noreply.github.com>
2019-03-07 23:41:16 +0300

    Update NOD.py

--- HW 25.02/NOD.py
+++ HW 25.02/NOD.py
@@ -6,12 +6,9 @@ def NOD(x,y):
             y=y%x
     return x+y
 
-amount=int(input("Введите количество повторов: "))
-
-while amount != 0:
-    first=int(input("\n\nВведите первое число: "))
-    second=int(input("Введите второе число: "))
-    if first==0 or second==0:
-        print("Число не должно быть равно нулю !!!")
-    else:
-        print("------------------\nНаибольший общий делитель данных чисел равен: ",NOD(first, second))
+first=int(input("\n\nВведите первое число: "))
+second=int(input("Введите второе число: "))
+if first==0 or second==0:
+    print("Число не должно быть равно нулю !!!")
+else:
+    print("------------------\nНаибольший общий делитель данных чисел равен: ",NOD(first, second))
